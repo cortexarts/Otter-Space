c2ab770c14393f3d26da421dbd3d2aba961ff641
JasperDre <j.drescher@cortexarts.com>
2016-12-15 13:51:16 +0100

    Removed old files and add sprites

---
+++ Reaching-Pluto/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene (int scene) {
+        SceneManager.LoadScene(scene);
+	}
+}
