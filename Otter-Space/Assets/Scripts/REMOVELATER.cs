7d76e0f127146a1c5f0d12697297ae810de50399
JasperDre <j.drescher@cortexarts.com>
2016-12-28 16:18:18 +0100

    Added starfields

--- Otter-Space/Assets/Scripts/REMOVELATER.cs
+++ Otter-Space/Assets/Scripts/REMOVELATER.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class REMOVELATER : MonoBehaviour {
 
     public float Speed = 0f;
+    public float rSpeed = 0f;
     private float movex = 0f;
     private float movey = 0f;
+    public float gravity = 0f;
+    bool Ongravity = false;
+
+    private Vector3 otherpos;
 
     // Use this for initialization
     void Start()
@@ -16,9 +21,22 @@ public class REMOVELATER : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetKey("space"))
+        {
+            if (rSpeed < 0.6f)
+            {
+                rSpeed += 0.2f * Time.deltaTime;
+            }
+            transform.Rotate(0, 0, rSpeed);
+        }
+        else
+        {
+            rSpeed = 0;
+        }
+
         if (movey > 0.0f)
         {
-            if (Speed < 6)
+            if (Speed < 5.0f)
             {
                 Speed += 0.8f * Time.deltaTime;
             }
@@ -26,5 +44,32 @@ public class REMOVELATER : MonoBehaviour {
         movex = Input.GetAxis("Horizontal");
         movey = Input.GetAxis("Vertical");
         GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
+
+        if (Ongravity)
+        {
+            if (gravity < 1.0f)
+            {
+                gravity += 0.4f * Time.deltaTime;
+            }
+            Vector2 direction = (otherpos - transform.position).normalized;
+            GetComponent<Rigidbody2D>().velocity = direction * gravity;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Planet")
+        {
+            otherpos = coll.transform.position;
+            Ongravity = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Planet")
+        {
+            Ongravity = false;
+            gravity = 0;
+        }
     }
 }
---
+++ Otter-Space/Assets/Scripts/Starfield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfield : MonoBehaviour {
+
+    public float parallax = 2.0f;
+
+    void Update() {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x / parallax;
+        offset.y = transform.position.y / transform.localScale.y / parallax;
+
+        mat.mainTextureOffset = offset;
+    }
+}
+
